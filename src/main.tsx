55d3d50a17aca2b2b19ce36dcea5576592558294
elinhalvorsen <elin_halvorsen99@outlook.com>
2024-02-19 18:07:17 +0100

    Added ol styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import MapApplication from "./components/MapApplication";
 import "./main.css";
+import "ol/ol.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <MapApplication />,
+    <MapApplication />
 );
