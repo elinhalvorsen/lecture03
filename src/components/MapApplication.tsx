3463e304081ad821d2a9135cee0c07e3a35116b1
elinhalvorsen <elin_halvorsen99@outlook.com>
2024-02-19 18:05:31 +0100

    Added styling

--- src/components/MapApplication.tsx
+++ src/components/MapApplication.tsx
@@ -1,6 +1,6 @@
+import React, { MutableRefObject, useRef, useEffect } from "react";
 import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
-import React, { MutableRefObject, useEffect, useRef } from "react";
 import { OSM } from "ol/source";
 import { useGeographic } from "ol/proj";
 
@@ -8,16 +8,20 @@ useGeographic();
 
 const map = new Map({
   layers: [new TileLayer({ source: new OSM() })],
-  view: new View({ center: [10, 59], zoom: 10 }),
+  view: new View({ center: [10, 59], zoom: 8 }),
 });
+
 const MapApplication = () => {
   const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
   useEffect(() => {
     map.setTarget(mapRef.current);
   }, []);
-
   return (
     <>
+      <header>
+        <h1>Kart</h1>
+      </header>
+      <nav></nav>
       <div ref={mapRef}>Map Should be here!!!</div>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import MapApplication from "./components/MapApplication";
+import "./main.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <MapApplication />,
 );
