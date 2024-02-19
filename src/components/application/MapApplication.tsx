dc521a6dcfe4b90acce9ba9622c08e445559dc8a
elinhalvorsen <elin_halvorsen99@outlook.com>
2024-02-19 20:58:01 +0100

    Show the name of the kommune

--- src/components/application/MapApplication.tsx
+++ src/components/application/MapApplication.tsx
@@ -1,33 +1,36 @@
-import React, { MutableRefObject, useRef, useEffect } from "react";
+import React, { MutableRefObject, useRef, useEffect, useState } from "react";
 import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import { OSM } from "ol/source";
 import { useGeographic } from "ol/proj";
 import KommuneLayer from "../kommuner/KommuneLayer";
+import Layer from "ol/layer/Layer";
 
 useGeographic();
 
 const map = new Map({
-  layers: [new TileLayer({ source: new OSM() })],
   view: new View({ center: [10, 59], zoom: 8 }),
 });
 
-const handleFocusUser = (e: React.MouseEvent) => {
-  e.preventDefault();
-  navigator.geolocation.getCurrentPosition((pos) => {
-    const { longitude, latitude } = pos.coords;
-    map.getView().animate({
-      center: [longitude, latitude],
-      zoom: 10,
-    });
-  });
-};
-
 const MapApplication = () => {
+  const handleFocusUser = (e: React.MouseEvent) => {
+    e.preventDefault();
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const { longitude, latitude } = pos.coords;
+      map.getView().animate({
+        center: [longitude, latitude],
+        zoom: 10,
+      });
+    });
+  };
+
+  const [layers, setLayers] = useState<Layer[]>([
+    new TileLayer({ source: new OSM() }),
+  ]);
+
   const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
-  useEffect(() => {
-    map.setTarget(mapRef.current);
-  }, []);
+  useEffect(() => map.setTarget(mapRef.current), []);
+  useEffect(() => map.setLayers(layers), [layers]);
   return (
     <>
       <header>
@@ -37,7 +40,7 @@ const MapApplication = () => {
         <a href={"#"} onClick={handleFocusUser}>
           Focus on me
         </a>
-        <KommuneLayer />
+        <KommuneLayer map={map} setLayers={setLayers} />
       </nav>
       <div ref={mapRef}>Map Should be here</div>
     </>
--- src/components/kommuner/KommuneLayer.tsx
+++ src/components/kommuner/KommuneLayer.tsx
@@ -1,6 +1,56 @@
-import React, { useState } from "react";
-const KommuneLayer = () => {
+import Layer from "ol/layer/Layer";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import { GeoJSON } from "ol/format";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { Feature, Map, MapBrowserEvent } from "ol";
+import { Polygon } from "ol/geom";
+
+type KommuneProperites = {
+  kommunenummer: string;
+  navn: {
+    sprak: string;
+    navn: string;
+  }[];
+};
+type KommuneFeactures = Feature<Polygon> & {
+  getProperties(): KommuneProperites;
+};
+const kommuneSource = new VectorSource<KommuneFeactures>({
+  url: "/lecture03/kommuner.json ",
+  format: new GeoJSON(),
+});
+const kommuneLayer = new VectorLayer({
+  source: kommuneSource,
+});
+const KommuneLayer = ({
+  setLayers,
+  map,
+}: {
+  map: Map;
+  setLayers: Dispatch<SetStateAction<Layer[]>>;
+}) => {
   const [checked, setChecked] = useState(false);
+
+  const handleClick = (e: MapBrowserEvent<MouseEvent>) => {
+    const clickedKommune = kommuneSource.getFeaturesAtCoordinate(
+      e.coordinate,
+    ) as KommuneFeactures[];
+    if (clickedKommune.length === 1) {
+      const properties = clickedKommune[0].getProperties() as KommuneProperites;
+      alert(properties.navn.find((n) => n.sprak === "nor")!.navn);
+    }
+  };
+  useEffect(() => {
+    if (checked) {
+      setLayers((old) => [...old, kommuneLayer]);
+      map.on("click", handleClick);
+    }
+    return () => {
+      map.un("click", handleClick);
+      setLayers((old) => old.filter((l) => l !== kommuneLayer));
+    };
+  }, [checked]);
   return (
     <>
       <label>
